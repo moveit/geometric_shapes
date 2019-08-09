c888ccd924b1207fdb6587a7fa27657546c30ca1
Martin Pecka <peckama2@fel.cvut.cz>
2019-08-09 15:42:17 -0400

    Refactored intersection tests for better readability.

--- test/test_ray_intersection.cpp
+++ test/test_ray_intersection.cpp
@@ -42,101 +42,118 @@
 #include <gtest/gtest.h>
 #include "resources/config.h"
 
+#define EXPECT_VECTORS_EQUAL(v1, v2, error)                                                                            \
+  EXPECT_NEAR((v1)[0], (v2)[0], (error));                                                                              \
+  EXPECT_NEAR((v1)[1], (v2)[1], (error));                                                                              \
+  EXPECT_NEAR((v1)[2], (v2)[2], (error));
+
+#define CHECK_NO_INTERSECTION(body, origin, direction)                                                                 \
+  {                                                                                                                    \
+    EigenSTL::vector_Vector3d intersections;                                                                           \
+    Eigen::Vector3d o origin;                                                                                          \
+    Eigen::Vector3d d direction;                                                                                       \
+    const auto result = (body).intersectsRay(o, d, &intersections, 2);                                                 \
+    EXPECT_FALSE(result);                                                                                              \
+    EXPECT_EQ(0, intersections.size());                                                                                \
+  }
+
+#define CHECK_INTERSECTS(body, origin, direction, numIntersections)                                                    \
+  {                                                                                                                    \
+    EigenSTL::vector_Vector3d intersections;                                                                           \
+    Eigen::Vector3d o origin;                                                                                          \
+    Eigen::Vector3d d direction;                                                                                       \
+    const auto result = (body).intersectsRay(o, d, &intersections, 2);                                                 \
+    EXPECT_TRUE(result);                                                                                               \
+    ASSERT_EQ((numIntersections), intersections.size());                                                               \
+  }
+
+#define CHECK_INTERSECTS_ONCE(body, origin, direction, intersection, error)                                            \
+  {                                                                                                                    \
+    EigenSTL::vector_Vector3d intersections;                                                                           \
+    Eigen::Vector3d o origin;                                                                                          \
+    Eigen::Vector3d d direction;                                                                                       \
+    Eigen::Vector3d i intersection;                                                                                    \
+    const auto result = (body).intersectsRay(o, d, &intersections, 2);                                                 \
+    EXPECT_TRUE(result);                                                                                               \
+    ASSERT_EQ(1, intersections.size());                                                                                \
+    EXPECT_VECTORS_EQUAL(intersections.at(0), i, (error));                                                             \
+  }
+
+#define CHECK_INTERSECTS_TWICE(body, origin, direction, intersc1, intersc2, error)                                     \
+  {                                                                                                                    \
+    EigenSTL::vector_Vector3d intersections;                                                                           \
+    Eigen::Vector3d o origin;                                                                                          \
+    Eigen::Vector3d d direction;                                                                                       \
+    Eigen::Vector3d i1 intersc1;                                                                                       \
+    Eigen::Vector3d i2 intersc2;                                                                                       \
+    const auto result = (body).intersectsRay(o, d, &intersections, 2);                                                 \
+    EXPECT_TRUE(result);                                                                                               \
+    ASSERT_EQ(2, intersections.size());                                                                                \
+    if (fabs(static_cast<double>((intersections.at(0) - i1).norm())) < (error))                                        \
+    {                                                                                                                  \
+      EXPECT_VECTORS_EQUAL(intersections.at(0), i1, (error));                                                          \
+      EXPECT_VECTORS_EQUAL(intersections.at(1), i2, (error));                                                          \
+    }                                                                                                                  \
+    else                                                                                                               \
+    {                                                                                                                  \
+      EXPECT_VECTORS_EQUAL(intersections.at(0), i2, (error));                                                          \
+      EXPECT_VECTORS_EQUAL(intersections.at(1), i1, (error));                                                          \
+    }                                                                                                                  \
+  }
+
 TEST(SphereRayIntersection, SimpleRay1)
 {
   shapes::Sphere shape(1.0);
-  bodies::Body* sphere = new bodies::Sphere(&shape);
-  sphere->setScale(1.05);
+  bodies::Sphere sphere(&shape);
+  sphere.setScale(1.05);
 
-  Eigen::Vector3d ray_o(5, 0, 0);
-  Eigen::Vector3d ray_d(-1, 0, 0);
-  EigenSTL::vector_Vector3d p;
-  bool intersect = sphere->intersectsRay(ray_o, ray_d, &p);
-
-  delete sphere;
-  EXPECT_TRUE(intersect);
-  EXPECT_EQ(2, (int)p.size());
-  EXPECT_NEAR(p[0].x(), 1.05, 1e-6);
-  EXPECT_NEAR(p[1].x(), -1.05, 1e-6);
+  CHECK_INTERSECTS_TWICE(sphere, (5, 0, 0), (-1, 0, 0), (1.05, 0, 0), (-1.05, 0, 0), 1e-6)
 }
 
 TEST(SphereRayIntersection, SimpleRay2)
 {
   shapes::Sphere shape(1.0);
-  bodies::Body* sphere = new bodies::Sphere(&shape);
-  sphere->setScale(1.05);
+  bodies::Sphere sphere(&shape);
+  sphere.setScale(1.05);
 
-  Eigen::Vector3d ray_o(5, 0, 0);
-  Eigen::Vector3d ray_d(1, 0, 0);
-  EigenSTL::vector_Vector3d p;
-  bool intersect = sphere->intersectsRay(ray_o, ray_d, &p);
-
-  delete sphere;
-  EXPECT_FALSE(intersect);
-  EXPECT_EQ(0, (int)p.size());
+  CHECK_NO_INTERSECTION(sphere, (5, 0, 0), (1, 0, 0))
 }
 
 TEST(BoxRayIntersection, SimpleRay1)
 {
   shapes::Box shape(1.0, 1.0, 3.0);
-  bodies::Body* box = new bodies::Box(&shape);
-  box->setScale(0.95);
+  bodies::Box box(&shape);
+  box.setScale(0.95);
 
-  Eigen::Vector3d ray_o(10, 0.449, 0);
-  Eigen::Vector3d ray_d(-1, 0, 0);
-  EigenSTL::vector_Vector3d p;
-
-  bool intersect = box->intersectsRay(ray_o, ray_d, &p);
-
-  //    for (unsigned int i = 0; i < p.size() ; ++i)
-  //        printf("intersection at %f, %f, %f\n", p[i].x(), p[i].y(), p[i].z());
-
-  delete box;
-  EXPECT_TRUE(intersect);
+  CHECK_INTERSECTS_TWICE(box, (10, 0.449, 0), (-1, 0, 0), (0.475, 0.449, 0), (-0.475, 0.449, 0), 1e-4)
 }
 
 TEST(BoxRayIntersection, SimpleRay2)
 {
   shapes::Box shape(0.9, 0.01, 1.2);
-  bodies::Body* box = new bodies::Box(&shape);
+  bodies::Box box(&shape);
 
   Eigen::Isometry3d pose(Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()));
   pose.translation() = Eigen::Vector3d(0, 0.005, 0.6);
-  box->setPose(pose);
+  box.setPose(pose);
 
-  Eigen::Vector3d ray_o(0, 5, 1.6);
-  Eigen::Vector3d ray_d(0, -5.195, -0.77);
-  EigenSTL::vector_Vector3d p;
+  const Eigen::Vector3d ray_d(0, -5.195, -0.77);
 
-  bool intersect = box->intersectsRay(ray_o, ray_d, &p);
-  EXPECT_TRUE(intersect);
-
-  intersect = box->intersectsRay(ray_o, ray_d.normalized(), &p);
-  EXPECT_TRUE(intersect);
-
-  delete box;
+  CHECK_INTERSECTS(box, (0, 5, 1.6), (ray_d.normalized()), 2)
 }
 
 TEST(BoxRayIntersection, SimpleRay3)
 {
   shapes::Box shape(0.02, 0.4, 1.2);
-  bodies::Body* box = new bodies::Box(&shape);
+  bodies::Box box(&shape);
 
   Eigen::Isometry3d pose(Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()));
   pose.translation() = Eigen::Vector3d(0.45, -0.195, 0.6);
-  box->setPose(pose);
+  box.setPose(pose);
 
-  Eigen::Vector3d ray_o(0, -2, 1.11);
-  Eigen::Vector3d ray_d(0, 1.8, -0.669);
-  EigenSTL::vector_Vector3d p;
+  const Eigen::Vector3d ray_d(0, 1.8, -0.669);
 
-  bool intersect = box->intersectsRay(ray_o, ray_d, &p);
-  EXPECT_FALSE(intersect);
-
-  intersect = box->intersectsRay(ray_o, ray_d.normalized(), &p);
-  EXPECT_FALSE(intersect);
-
-  delete box;
+  CHECK_NO_INTERSECTION(box, (0, -2, 1.11), (ray_d.normalized()))
 }
 
 int main(int argc, char** argv)
