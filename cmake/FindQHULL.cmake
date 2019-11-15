eae9fb01fc4b0513504f913e3082ba1ffdeb143d
Henning Kayser <henningkayser@picknik.ai>
2019-11-15 11:36:48 +0100

    Fix ament_lint_cmake

--- cmake/FindQHULL.cmake
+++ cmake/FindQHULL.cmake
@@ -5,7 +5,7 @@
 # QHULL_FOUND - True if QHULL was found.
 # QHULL_INCLUDE_DIRS - Directories containing the QHULL include files.
 # QHULL_LIBRARIES - Libraries needed to use QHULL.
-# If QHULL_USE_STATIC is specified then look for static libraries ONLY else 
+# If QHULL_USE_STATIC is specified then look for static libraries ONLY else
 # look for shared ones
 
 set(QHULL_MAJOR_VERSION 6)
@@ -13,18 +13,18 @@ set(QHULL_MAJOR_VERSION 6)
 if(QHULL_USE_STATIC)
   set(QHULL_RELEASE_NAME qhullstatic)
   set(QHULL_DEBUG_NAME qhullstatic_d)
-else(QHULL_USE_STATIC)
+else()
   set(QHULL_RELEASE_NAME qhull_p qhull${QHULL_MAJOR_VERSION} qhull)
   set(QHULL_DEBUG_NAME qhull_pd qhull${QHULL_MAJOR_VERSION}_d qhull_d${QHULL_MAJOR_VERSION} qhull_d)
-endif(QHULL_USE_STATIC)
+endif()
 
 find_file(QHULL_HEADER
           NAMES libqhull/libqhull.h qhull.h
           HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}" "${QHULL_INCLUDE_DIR}"
-          PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull" 
+          PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull"
           PATH_SUFFIXES qhull src/libqhull libqhull include)
 
-set(QHULL_HEADER "${QHULL_HEADER}" CACHE INTERNAL "QHull header" FORCE )
+set(QHULL_HEADER "${QHULL_HEADER}" CACHE INTERNAL "QHull header" FORCE)
 
 if(QHULL_HEADER)
   get_filename_component(qhull_header ${QHULL_HEADER} NAME_WE)
@@ -36,33 +36,33 @@ if(QHULL_HEADER)
     get_filename_component(QHULL_INCLUDE_DIR ${QHULL_HEADER} PATH)
     get_filename_component(QHULL_INCLUDE_DIR ${QHULL_INCLUDE_DIR} PATH)
   endif()
-else(QHULL_HEADER)
+else()
   set(QHULL_INCLUDE_DIR "QHULL_INCLUDE_DIR-NOTFOUND")
-endif(QHULL_HEADER)
+endif()
 
 set(QHULL_INCLUDE_DIR "${QHULL_INCLUDE_DIR}" CACHE PATH "QHull include dir." FORCE)
 
-find_library(QHULL_LIBRARY 
-             NAMES ${QHULL_RELEASE_NAME}
-             HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
-             PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull" 
-             PATH_SUFFIXES project build bin lib)
+find_library(QHULL_LIBRARY
+  NAMES ${QHULL_RELEASE_NAME}
+  HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
+  PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull"
+  PATH_SUFFIXES project build bin lib)
 
-find_library(QHULL_LIBRARY_DEBUG 
-             NAMES ${QHULL_DEBUG_NAME} ${QHULL_RELEASE_NAME}
-             HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
-             PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull" 
-             PATH_SUFFIXES project build bin lib)
+find_library(QHULL_LIBRARY_DEBUG
+  NAMES ${QHULL_DEBUG_NAME} ${QHULL_RELEASE_NAME}
+  HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}"
+  PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull"
+  PATH_SUFFIXES project build bin lib)
 
 if(NOT QHULL_LIBRARY_DEBUG)
   set(QHULL_LIBRARY_DEBUG ${QHULL_LIBRARY})
-endif(NOT QHULL_LIBRARY_DEBUG)
+endif()
 
 set(QHULL_INCLUDE_DIRS ${QHULL_INCLUDE_DIR})
 set(QHULL_LIBRARIES optimized ${QHULL_LIBRARY} debug ${QHULL_LIBRARY_DEBUG})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Qhull DEFAULT_MSG QHULL_LIBRARY QHULL_INCLUDE_DIR)
+find_package_handle_standard_args(QHULL DEFAULT_MSG QHULL_LIBRARY QHULL_INCLUDE_DIR)
 
 mark_as_advanced(QHULL_LIBRARY QHULL_LIBRARY_DEBUG QHULL_INCLUDE_DIR)
 
@@ -72,7 +72,7 @@ if(QHULL_FOUND)
     add_definitions("-Dqh_QHpointer")
     if(MSVC)
       add_definitions("-Dqh_QHpointer_dllimport")
-    endif(MSVC)
-  endif(NOT QHULL_USE_STATIC)
+    endif()
+  endif()
   message(STATUS "QHULL found (include: ${QHULL_INCLUDE_DIRS}, lib: ${QHULL_LIBRARIES})")
-endif(QHULL_FOUND)
+endif()
