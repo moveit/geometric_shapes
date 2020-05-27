4c8b6d43ac7df54d3e07015558b65cf1e235070a
AndyZe <andyz@utexas.edu>
2020-05-27 16:45:49 -0500

    Allow a larger tolerance on matrix scaling

--- include/geometric_shapes/check_isometry.h
+++ include/geometric_shapes/check_isometry.h
@@ -57,7 +57,7 @@
 
 #ifndef CHECK_ISOMETRY_PRECISION
 /** \brief The default precision to which the transform has to correspond to an isometry. */
-#define CHECK_ISOMETRY_PRECISION Eigen::NumTraits<double>::dummy_precision()
+#define CHECK_ISOMETRY_PRECISION 1e-6
 #endif
 
 /**
