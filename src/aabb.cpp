b592ebcc3ea4df717847f13306c42cac47d10d29
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-08-25 08:45:10 +0200

    fixup! Future-proof preprocessor checks for FCL version.

--- src/aabb.cpp
+++ src/aabb.cpp
@@ -43,7 +43,7 @@
 
 void bodies::AABB::extendWithTransformedBox(const Eigen::Isometry3d& transform, const Eigen::Vector3d& box)
 {
-#if FCL_MAJOR_VERSION == 0 && FCL_MINOR_VERSION == 5
+#if FCL_MAJOR_VERSION == 0 && FCL_MINOR_VERSION <= 5
   // Method adapted from FCL src/shape/geometric_shapes_utility.cpp#computeBV<AABB, Box>(...) (BSD-licensed code):
   // https://github.com/flexible-collision-library/fcl/blob/fcl-0.4/src/shape/geometric_shapes_utility.cpp#L292
   // We don't call their code because it would need creating temporary objects, and their method is in floats in FCL 0.5
