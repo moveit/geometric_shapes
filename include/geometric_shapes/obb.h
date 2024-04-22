520d5a5cffd71456323216615594898044b7e709
Sebastian Pelletier <37712986+spelletier1996@users.noreply.github.com>
2024-04-22 17:24:35 -0400

    Copyright year
    Co-authored-by: Martin Pecka <peci1@seznam.cz>

--- include/geometric_shapes/obb.h
+++ include/geometric_shapes/obb.h
@@ -1,4 +1,4 @@
-// Copyright 2019 Open Robotics
+// Copyright 2024 Open Robotics
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
